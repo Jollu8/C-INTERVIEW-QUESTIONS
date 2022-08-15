50f26df120abf8cc7e58b18ed7ff2f91580b362e
Jollu8 <jollu8@gmail.com>
2022-08-15 17:36:23 +0300

    !

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
-//
-// Created by jollu.
-//
-//
-
-
+#include <fstream>
+#include <string>
 #include <iostream>
+#include <algorithm>
+#include <set>
+
 
 int main() {
-    std::cout << "Hello world";
+    std::ifstream input("input.txt");
+    std::string a, b;
+    input >> a >> b;
+    input.close();
+    std::set<char> s(a.begin(), a.end());
+    int n = 0;
+    for(auto i : s) {
+        n += std::count(b.begin(), b.end(), i);
+    }
+    std::ofstream output("output.txt");
 
+    output << n;
+    output.close();
 }
