39b36459ace3db8a95849c7590a408816239208f
Jollu8 <jollu8@gmail.com>
2022-08-20 22:28:24 +0300

    !

--- main.cpp
+++ main.cpp
@@ -6,52 +6,42 @@
 #include <vector>
 #include <memory>
 
-class ListNode
-{
+class ListNode {
 public:
-int val;
-ListNode *next;
+    int val;
+    ListNode *next;
 
-~ListNode()
-{
-}
+    ~ListNode() {
+    }
 
 };
 
 
-std::shared_ptr<ListNode> createListNode(int digit)
-{
+std::shared_ptr<ListNode> createListNode(int digit) {
     std::string dig = std::to_string(digit);
 
     std::shared_ptr<ListNode> listNode = std::make_shared<ListNode>();
-    ListNode * ln = listNode.get();
+    ListNode *ln = listNode.get();
 
-    for(int i = dig.size() - 1; i >= 0; i--)
-    {
+    for (int i = dig.size() - 1; i >= 0; i--) {
         ln->val = dig[i] - '0';
 
-        if(i != 0)
-        {
+        if (i != 0) {
             ln->next = new ListNode();
             ln = ln->next;
-        }
-        else ln->next = nullptr;
+        } else ln->next = nullptr;
     }
 
     return listNode;
 }
 
 int main() {
-    int a  = 19;
-    int b = 17;
-    int max = (a > b) ? a : b;
-    int res;
-    for (;;) {
-        if (max % a == 0 && max % b == 0) {
-            res = max;
+    int a = 28;
+    int b = 5;
+    for (auto i = 28;; i++) {
+        if (i % a == 0 && i % b == 0) {
+            std::cout << i << std::endl;
             break;
         }
-        max++;
     }
-    std::cout << res;
 }
