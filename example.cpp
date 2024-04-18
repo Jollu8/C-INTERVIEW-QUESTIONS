7bd018a432cd2e16e4f5fa73b166599100c15bba
Zholboldu Emilbekuulu <z.emilbekuulu@adguard.com>
2024-04-18 19:50:29 +0300

    restore old files

--- example.cpp
+++ example.cpp
@@ -1,10 +1,100 @@
 #include <vector>
 #include <iostream>
+#include <queue>
+#include <stack>
 
-class Queue {
+#include <iostream>
 
+using namespace std;
+
+struct Node {
+    int val;
+    std::vector<Node *> children;
+
+    Node() {
+        val = 0;
+    }
+
+    Node(int n, int d): val(d), children(n, nullptr) {
+    }
 };
 
-int main(){
+void inorder(Node *h) {
+    if (!h) return;
 
+    for (auto &i: h->children) {
+        inorder(i);
+    }
+    std::cout << h->val << " ";
+}
+
+void preorder(Node *root) {
+    stack<Node *> s;
+    vector<int> pre;
+    s.push(root);
+
+    while (!s.empty()) {
+        Node *temp = s.top();
+        s.pop();
+        pre.push_back(temp->val);
+        for (auto *i: temp->children) {
+            if (i)
+                s.push(i);
+        }
+    }
+    for (auto &i: pre) {
+        std::cout << i << " ";
+    }
+}
+
+void post_stack(Node *root, vector<int> &ans) {
+    stack<Node*> s;
+    s.push(root);
+    while(!s.empty()) {
+        auto temp = s.top();
+        s.pop();
+        ans.push_back(root->val);
+        for(auto *n : temp->children) if(n) s.push(n);
+    }
+}
+
+
+class Solution {
+    [[maybe_unused]] vector<int> preorder_loop(Node *root) {
+        if (!root) return {};
+        vector<int> ans;
+        stack<Node *> s;
+        s.push(root);
+        while (!s.empty()) {
+            auto *temp = s.top();
+            s.pop();
+            ans.push_back(temp->val);
+            auto it = temp->children.rbegin();
+            for (; it != temp->children.rend(); ++it) {
+                if (*it)
+                    s.push(*it);
+            }
+        }
+        return ans;
+    }
+
+    void preorder_recur(Node *root, vector<int> &ans) {
+        if(!root) return;
+
+        ans.push_back(root->val);
+        for(auto *n : root->children) preorder_recur(n, ans);
+    }
+
+public:
+    vector<int> preorder(Node *root) {
+        if (!root) return {};
+    }
+};
+
+
+
+
+
+int main() {
+   std::cout  << std::max({1,3,-5,7}) + std::min({-3,6,-8,-1}) + std::abs(-17);
 }
---
+++ start.md
@@ -0,0 +1,30 @@
+# Вот полный гид для вас
+
+# Важно!
+
+Чтобы быстро стать профессионалом, не делайте следующее:
+1. Копипаста
+2. Откладывание
+3. Прыжки с места (если что-то не получается, отложите и вернитесь к этому через некоторое время)
+4. Чтение всех больших книг
+---
+
+## 1. Основы языка
+- Прежде всего, перейдите по [ссылке](https://www.hackerrank.com/domains/cpp?filters%5Bstatus%5D%5B%5D=unsolved&badge_type=cpp). Если что-то не получается, прочитайте [гид](./readme.md) по остальным темам.
+
+## 2. DSA
+- Как только вы освоите основы языка, перейдите по этой [ссылке](https://leetcode.com/explore/learn/card/the-leetcode-beginners-guide/679/sql-syntax/4358/).
+- Если некоторые материалы доступны только для премиум-аккаунта, переходите по этим ссылкам.
+- [graph](https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/)
+- вот еще [мап](https://neetcode.io/roadmap)
+
+
+## 3. SQL
+- Выполните все эти [задачи](https://leetcode.com/explore/learn/card/sql-language/).
+
+## 4. Проектирование систем
+- Перейдите по этой [ссылке](https://leetcode.com/explore/learn/card/system-design/).
+- здесь [бесплатно](https://leetdesign.com/library/introduction)
+
+# 5. Работа
+- Напишите мне в [Telegram](t.me/LordJollu). Я предоставлю материалы о том, как составить резюме, за чашечку кофе =)
